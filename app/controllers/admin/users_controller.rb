fb79c3a3c9f6c943b0b2dd8a224d3f5777d82791
Paul Raymond Manabat <paulraymondmanabat@gmail.com>
2017-03-10 19:46:59 +0800

    Add New User

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,2 +1,16 @@
 class Admin::UsersController < AdminController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    @user.save
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:first_name, :last_name, :nickname, :email, :role, :password)
+    end
 end
