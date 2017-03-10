a3b0cc889450ead0f561415422d60155a2e88204
Daryl Joe Yao <djy@sourcepad.com>
2017-03-10 19:59:24 +0800

    redesign hall page

---
+++ app/assets/javascripts/admin/settings.coffee
@@ -0,0 +1,6 @@
+$(document).ready ->
+  setTimeout (->
+    $('#bg-setting-tooltip').fadeOut('slow')
+    ), 2000
+
+  return
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,4 +14,5 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap-sprockets
+//= require jquery-ui
 //= require_tree .
