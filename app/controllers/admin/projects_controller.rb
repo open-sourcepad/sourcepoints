e67739c7861681c166036d6d1c837935fda84663
Paul Raymond Manabat <paulraymondmanabat@gmail.com>
2017-03-10 20:00:10 +0800

    Add New Project

--- app/controllers/admin/projects_controller.rb
+++ app/controllers/admin/projects_controller.rb
@@ -1,2 +1,16 @@
 class Admin::ProjectsController < AdminController
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+    @project.save
+  end
+
+  private
+    def project_params
+      params.require(:project).permit(:name)
+    end
 end
